49167a3d97d4e3497fb3463de61d508d4abafc92
Marc Neudert <marc.neudert@gmail.com>
2019-09-20 17:53:53 +0200

    Please the formatting gods

--- lib/maxmind_csv/schema/blocks.ex
+++ lib/maxmind_csv/schema/blocks.ex
@@ -4,14 +4,14 @@ defmodule Geolix.Adapter.MaxMindCSV.Schema.Blocks do
   use Ecto.Schema
 
   schema "geolix_maxmind_csv_blocks" do
-    field(:network_lower, :decimal)
-    field(:network_upper, :decimal)
-    field(:geoname_id, :integer)
-    field(:registered_country_geoname_id, :integer)
-    field(:represented_country_geoname_id, :integer)
-    field(:postal_code, :string)
-    field(:latitude, :decimal)
-    field(:longitude, :decimal)
-    field(:accuracy_radius, :integer)
+    field :network_lower, :decimal
+    field :network_upper, :decimal
+    field :geoname_id, :integer
+    field :registered_country_geoname_id, :integer
+    field :represented_country_geoname_id, :integer
+    field :postal_code, :string
+    field :latitude, :decimal
+    field :longitude, :decimal
+    field :accuracy_radius, :integer
   end
 end
