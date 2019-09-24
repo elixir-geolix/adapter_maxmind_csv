bf0e66f6d99d5f82d2ddb44b06ec7d2f0841d770
Marc Neudert <marc.neudert@gmail.com>
2019-09-24 19:52:20 +0200

    Retrieves basic database entry upon lookup request

--- lib/maxmind_csv.ex
+++ lib/maxmind_csv.ex
@@ -12,12 +12,32 @@ defmodule Geolix.Adapter.MaxMindCSV do
           %{
             id: :my_csv_database,
             adapter: Geolix.Adapter.MaxMindCSV
+            type: :city
           }
         ]
   """
 
+  import Ecto.Query, only: [where: 3]
+
+  alias Geolix.Adapter.MaxMindCSV.IP
+  alias Geolix.Adapter.MaxMindCSV.Repo
+  alias Geolix.Adapter.MaxMindCSV.Schema.ASNBlock
+  alias Geolix.Adapter.MaxMindCSV.Schema.CityBlock
+  alias Geolix.Adapter.MaxMindCSV.Schema.CountryBlock
+
   @behaviour Geolix.Adapter
 
   @impl Geolix.Adapter
-  def lookup(_ip, _opts, _database), do: nil
+
+  def lookup(ip, _opts, %{type: :asn}), do: perform_lookup(ip, ASNBlock)
+  def lookup(ip, _opts, %{type: :city}), do: perform_lookup(ip, CityBlock)
+  def lookup(ip, _opts, %{type: :country}), do: perform_lookup(ip, CountryBlock)
+
+  defp perform_lookup(ip, schema) do
+    ip_integer = IP.to_integer(ip)
+
+    schema
+    |> where([b], b.network_lower <= ^ip_integer and b.network_upper >= ^ip_integer)
+    |> Repo.one()
+  end
 end
--- test/maxmind_csv_test.exs
+++ test/maxmind_csv_test.exs
@@ -1,13 +1,74 @@
 defmodule Geolix.Adapter.MaxMindCSVTest do
-  use ExUnit.Case
+  use Geolix.Adapter.MaxMindCSV.RepoCase
 
-  test "lookup" do
+  alias Geolix.Adapter.MaxMindCSV.Schema.ASNBlock
+  alias Geolix.Adapter.MaxMindCSV.Schema.CityBlock
+  alias Geolix.Adapter.MaxMindCSV.Schema.CountryBlock
+
+  test "lookup :asn" do
+    result =
+      Repo.insert!(%ASNBlock{
+        network_lower: Decimal.new(16_843_008),
+        network_upper: Decimal.new(16_843_263),
+        autonomous_system_number: 1,
+        autonomous_system_organization: "test"
+      })
+
     database = %{
-      id: :csv_lookup,
-      adapter: Geolix.Adapter.MaxMindCSV
+      id: :csv_lookup_asn,
+      adapter: Geolix.Adapter.MaxMindCSV,
+      type: :asn
     }
 
     assert :ok == Geolix.load_database(database)
-    assert nil == Geolix.lookup({1, 1, 1, 1}, where: database[:id])
+    assert result == Geolix.lookup({1, 1, 1, 1}, where: database[:id])
+  end
+
+  test "lookup :city" do
+    result =
+      Repo.insert!(%CityBlock{
+        network_lower: Decimal.new(16_843_008),
+        network_upper: Decimal.new(16_843_263),
+        geoname_id: 1,
+        registered_country_geoname_id: 1,
+        represented_country_geoname_id: 1,
+        is_anonymous_proxy: false,
+        is_satellite_provider: false,
+        postal_code: "12345",
+        latitude: Decimal.from_float(0.123),
+        longitude: Decimal.from_float(0.123),
+        accuracy_radius: 100
+      })
+
+    database = %{
+      id: :csv_lookup_city,
+      adapter: Geolix.Adapter.MaxMindCSV,
+      type: :city
+    }
+
+    assert :ok == Geolix.load_database(database)
+    assert result == Geolix.lookup({1, 1, 1, 1}, where: database[:id])
+  end
+
+  test "lookup :country" do
+    result =
+      Repo.insert!(%CountryBlock{
+        network_lower: Decimal.new(16_843_008),
+        network_upper: Decimal.new(16_843_263),
+        geoname_id: 1,
+        registered_country_geoname_id: 1,
+        represented_country_geoname_id: 1,
+        is_anonymous_proxy: false,
+        is_satellite_provider: false
+      })
+
+    database = %{
+      id: :csv_lookup_country,
+      adapter: Geolix.Adapter.MaxMindCSV,
+      type: :country
+    }
+
+    assert :ok == Geolix.load_database(database)
+    assert result == Geolix.lookup({1, 1, 1, 1}, where: database[:id])
   end
 end
