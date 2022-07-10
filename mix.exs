9b02ad93b88154f17019b66b79c833b45b46d69d
Marc Neudert <marc.neudert@gmail.com>
2022-07-10 12:08:28 +0200

    Update GitHub dialyzer configuration

--- mix.exs
+++ mix.exs
@@ -56,7 +56,7 @@ defmodule Geolix.Adapter.MaxMindCSV.Mixfile do
       ],
       plt_add_apps: [:ecto_ip_range, :ecto_network],
       plt_core_path: "plts",
-      plt_file: {:no_warn, "plts/dialyzer.plt"}
+      plt_local_path: "plts"
     ]
   end
 
